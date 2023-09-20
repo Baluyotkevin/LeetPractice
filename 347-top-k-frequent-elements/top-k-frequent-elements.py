f797ac465960e7a62c9440207dfd35197f17fb71
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-09-20 14:45:27 -0700

    Time: 424 ms (5.00%) | Memory: 20.8 MB (95.43%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # reset check back to 0 each time its count >= 2
+        # append if count is >= 2
+        # return
+        answer = []
+        check = {}
+        for el in nums:
+            if el not in check:
+                check[el] = 1
+            else:
+                check[el] += 1
+        lis = []
+        for num in check.values():
+            lis.append(num)
+        lis.sort()
+        lis = lis[-k:]
+        for i in lis[:k]:
+            for k in check:
+                if check[k] == i and k not in answer:
+                    answer.append(k)
+        return answer
+
