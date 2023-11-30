5b6a8803fd86549469876d7c49bcabf393db08d6
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-11-29 16:31:04 -0800

    Time: 42 ms (23.78%) | Memory: 16.3 MB (11.75%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,15 +1,22 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        words = {}
-        ans = []
-        temp = 0
-        for i in range(len(s) - 1, -1, -1):
-            if s[i].isdigit():
-                temp = s[i]
-                words[s[i]] = ''
-            elif s[i].isalpha():
-                words[temp] = s[i] + words[temp]
-        word_sort = sorted(words.items())
-        for j in range(len(word_sort)):
-            ans.append(word_sort[j][1])
+        lis = s.split(' ')
+        ans = [0] * len(lis)
+        for i in range(len(lis)):
+            idx = int(lis[i][-1])
+            word = lis[i][0:-1]
+            ans[idx - 1] = word
         return ' '.join(ans)
+        # words = {}
+        # ans = []
+        # temp = 0
+        # for i in range(len(s) - 1, -1, -1):
+        #     if s[i].isdigit():
+        #         temp = s[i]
+        #         words[s[i]] = ''
+        #     elif s[i].isalpha():
+        #         words[temp] = s[i] + words[temp]
+        # word_sort = sorted(words.items())
+        # for j in range(len(word_sort)):
+        #     ans.append(word_sort[j][1])
+        # return ' '.join(ans)
