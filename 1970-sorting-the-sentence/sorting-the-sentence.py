899a6a63b803ada3152e9773d3f2c421999f0f25
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-11-29 16:31:20 -0800

    Time: 34 ms (80.60%) | Memory: 16.3 MB (45.35%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -7,16 +7,3 @@ class Solution:
             word = lis[i][0:-1]
             ans[idx - 1] = word
         return ' '.join(ans)
-        # words = {}
-        # ans = []
-        # temp = 0
-        # for i in range(len(s) - 1, -1, -1):
-        #     if s[i].isdigit():
-        #         temp = s[i]
-        #         words[s[i]] = ''
-        #     elif s[i].isalpha():
-        #         words[temp] = s[i] + words[temp]
-        # word_sort = sorted(words.items())
-        # for j in range(len(word_sort)):
-        #     ans.append(word_sort[j][1])
-        # return ' '.join(ans)
