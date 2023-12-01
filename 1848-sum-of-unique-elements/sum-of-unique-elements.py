91f31e05a0ff0a180c52d559d03326cd8b9a29a6
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-12-01 12:43:22 -0800

    Time: 39 ms (74.37%) | Memory: 16.3 MB (10.22%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -1,13 +1,8 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        obj = {}
+        obj = defaultdict(int)
+
         for num in nums:
-            if num not in obj: 
-                obj[num] = 1
-            else:
-                obj[num] += 1
-        ans = 0
-        for value, count in obj.items():
-            if count == 1:
-                ans += value
-        return ans
+            obj[num] += 1
+
+        return sum(value for value, count in obj.items() if count == 1)
