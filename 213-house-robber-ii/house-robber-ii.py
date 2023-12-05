9bcf1cfe0d2d53337964471b712b5d6b36c7a005
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-12-05 10:04:17 -0800

    Time: 42 ms (40.94%) | Memory: 16.2 MB (83.17%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
         def robber(arr):
             rob1, rob2 = 0, 0
             for n in arr:
@@ -8,4 +7,4 @@ class Solution:
                 rob1 = rob2
                 rob2 = temp
             return rob2
-        return max(robber(nums[:-1]), robber(nums[1:]))
+        return max(nums[0], robber(nums[:-1]), robber(nums[1:]))
