c978874f031ce05783e03bb34edbebd4237105aa
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-12-17 11:35:49 -0800

    Time: 904 ms (18.31%) | Memory: 27.3 MB (42.99%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # going to have two pointers
-        # if pointer one is less than two then pointer one stays the same and pointer two increases other wise pointer one is now pointer two and pointer two increases
-        # if right pointer
-        # another variable will be our max
-        left = 0
-        right = 1
-        max_sum = 0
-        while right < len(prices):
-            if prices[left] > prices[right]:
-                left = right
-            print(prices[left], prices[right])
-            if prices[right] - prices[left] > max_sum:
-                max_sum = prices[right] - prices[left]
-            right+=1
-        return max_sum
-
-        
+        l, r = 0, 1
+        max_profit = 0
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+            profit = prices[r] - prices[l]
+            max_profit = max(profit, max_profit)
+            r += 1
+        return max_profit
