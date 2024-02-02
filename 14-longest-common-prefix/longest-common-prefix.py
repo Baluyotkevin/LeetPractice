3ea52a630d431ff0742e4681b3490c40bcfd1969
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-02-02 07:03:36 -0800

    Time: 45 ms (27.50%) | Memory: 16.7 MB (61.99%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -3,8 +3,8 @@ class Solution:
         compare = strs[0]
         res = ""
         for i in range(len(compare)):
-            for s in strs:
-                if i == len(s) or s[i] != compare[i]:
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != compare[i]:
                     return res
             res+= compare[i]
         return res
