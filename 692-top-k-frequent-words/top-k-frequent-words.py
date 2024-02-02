bb5c318f45383b6cde86c06ae0655e6554c9c122
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-02-02 13:04:16 -0800

    Time: 60 ms (59.74%) | Memory: 16.7 MB (69.07%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,12 +1,24 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # gives a dict where its a counter of how much a word appears
-        words_dict = Counter(words)
-        # turns the dict into an array with counter as the first ele and word as the second
-        items = words_dict.items()
-        # sorts the array of words/numbers from desc to asc
-        sorted_items = sorted(items, key=lambda x: (-x[1], x[0]))
-        # now we just want the words and since they are sorted from desc to asc
-        sorted_keys = [x[0] for x in sorted_items]
-        # we now return the array of sorted keys up til however many k wants
-        return sorted_keys[:k]
+        # dictionary word and frequency values
+        # iterate through list
+        dic = {}
+        for word in words:
+            if word not in dic:
+                dic[word] = 1
+            else:
+                dic[word] += 1
+        sort = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
+        ans = []
+        i = 0
+        while i < k:
+            ans.append(sort[i][0])
+            i += 1
+        return ans
+
+
+        # arr = []
+        # for k, v in dic.items():
+        #     arr.append([k, v])
+        # sorted(arr, key=)
+            
