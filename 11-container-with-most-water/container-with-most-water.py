8514e2889ef12d9e9325a4c1635903826c6ac524
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-02-09 10:45:50 -0800

    Time: 552 ms (36.61%) | Memory: 30.2 MB (30.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        area = 0
-        while left < right:
-            new_area = (right - left) * min(height[left], height[right])
-            area = max(area, new_area)
-            if height[left] < height[right]:
-                left+=1
+        # two pointer where we have
+        # left pointer and right pointer max num()
+        # variable to keep track of length because we want the max length we can get but also the highest height
+        # we would keep track of the max(area)
+        # left <= right
+        l, r = 0, len(height) - 1
+        max_area = float('-inf')
+        while l < r:
+            area = (r - l) * min(height[l], height[r])
+            max_area = max(max_area, area)
+            if height[l] < height[r]:
+                l += 1
             else:
-                right-=1
-        return area
+                r -= 1
+        return max_area
+            
