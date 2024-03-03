9c1c2e23a0270adf91a45e2e7c4dad0027e25919
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-03-03 14:59:03 -0800

    Time: 35 ms (72.91%) | Memory: 16.4 MB (86.64%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -4,7 +4,11 @@ class Solution:
         string = n
         while string != 1 and string not in seen:
             seen.add(string)
-            string = sum([int(num)**2 for num in str(string)])
+            string = str(string)
+            tot = 0
+            for i in range(len(string)):
+                tot += int(string[i])**2
+            string = tot
         return True if string == 1 else False
                 
                 
