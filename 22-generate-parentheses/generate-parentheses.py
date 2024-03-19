b110f415d8fa6d2807d3b235f3139de11287628d
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-03-19 14:16:14 -0700

    Time: 32 ms (87.13%) | Memory: 16.8 MB (43.12%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,10 +3,9 @@ class Solution:
         def dps(combo, memo, left, right):
             if left == 0 and right == 0:
                 memo.append(combo)
-            
-            if left > 0:
+            if left != 0:
                 dps(combo + '(', memo, left - 1, right)
-            if right > 0 and left < right:
+            if left < right:
                 dps(combo + ')', memo, left, right - 1)
             
 
