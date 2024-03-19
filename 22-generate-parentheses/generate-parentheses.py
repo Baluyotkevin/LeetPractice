25582c6f98bb05e3de32e009d126de4cd30b8ec4
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-03-19 14:10:50 -0700

    Time: 32 ms (87.13%) | Memory: 16.7 MB (89.76%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def dps(combo, memo, left, right):
+            if left == 0 and right == 0:
+                memo.append(combo)
+            
+            if left > 0:
+                dps(combo + '(', memo, left - 1, right)
+            if right > 0 and left < right:
+                dps(combo + ')', memo, left, right - 1)
+            
+
+        memo = []
+        dps('', memo, n, n)
+        return memo
