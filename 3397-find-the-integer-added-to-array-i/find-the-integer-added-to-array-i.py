e02f6e8a1f75f9eb4417e0fba4c0457fdd827b04
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-05-01 10:34:30 -0700

    Time: 45 ms (82.82%) | Memory: 16.6 MB (18.29%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -3,16 +3,7 @@ class Solution:
         # sort so that we can identify the integer x
         # iterate through nums1 and find out the difference between all of them
         # should figure out the occurances
-        obj = {}
         nums1.sort()
         nums2.sort()
-        total = 0
-        for i in range(len(nums1)):
-            diff = nums2[i] - nums1[i]
-            total = diff
-            if diff not in obj:
-                obj[diff] = 1
-            else:
-                obj[diff] += 1
-            return total
-            
+        return nums2[0] - nums1[0]
+  
